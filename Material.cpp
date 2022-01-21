221fe9f48c94ac2e832d8a69c97b570de34be515
AliyevRufat <76409612+AliyevRufat@users.noreply.github.com>
2022-01-21 17:22:33 +0100

    Created a better showcase scene

--- Material.cpp
+++ Material.cpp
@@ -12,7 +12,7 @@ Material_Lambert::Material_Lambert(float diffuseReflectance, const  Elite::RGBCo
 	m_DiffuseReflectance = { diffuseReflectance };
 }
 
-Elite::RGBColor Material_Lambert::Shade(const HitRecord& hitRecord, const Elite::FVector3& l, const Elite::FVector3& v, bool& isShade,const Elite::FVector3& incidentRay) const
+Elite::RGBColor Material_Lambert::Shade(const HitRecord& hitRecord, const Elite::FVector3& l, const Elite::FVector3& v, bool& isRefractive,const Elite::FVector3& incidentRay) const
 {
 	return BRDF::Lambert(m_DiffuseReflectance, m_DiffuseColor);
 }
@@ -25,7 +25,7 @@ Material_LambertPhong::Material_LambertPhong(float diffuseReflectance, const Eli
 	m_DiffuseReflectance = diffuseReflectance;
 }
 
-Elite::RGBColor Material_LambertPhong::Shade(const HitRecord& hitRecord, const Elite::FVector3& l, const Elite::FVector3& v, bool& isShade, const Elite::FVector3& incidentRay) const
+Elite::RGBColor Material_LambertPhong::Shade(const HitRecord& hitRecord, const Elite::FVector3& l, const Elite::FVector3& v, bool& isRefractive, const Elite::FVector3& incidentRay) const
 {
 	return BRDF::Lambert(m_DiffuseReflectance, m_DiffuseColor) +
 		BRDF::Phong(m_SpecularReflectance, m_PhongExponent, l, v, hitRecord.normal);
@@ -41,7 +41,7 @@ Material_CookTorrance::Material_CookTorrance(float diffuseReflectance, const Eli
 	m_DiffuseReflectance = diffuseReflectance;
 }
 
-Elite::RGBColor Material_CookTorrance::Shade(const HitRecord& hitRecord, const Elite::FVector3& l, const Elite::FVector3& v, bool& isShade, const Elite::FVector3& incidentRay) const
+Elite::RGBColor Material_CookTorrance::Shade(const HitRecord& hitRecord, const Elite::FVector3& l, const Elite::FVector3& v, bool& isRefractive, const Elite::FVector3& incidentRay) const
 {
 	Elite::FVector3 halfVector = (v + l) / (Elite::Magnitude(v + l));
 	Elite::RGBColor F = m_F0 + (Elite::RGBColor(1, 1, 1) - m_F0) * (pow(1 - (Elite::Dot(halfVector, v)), 5));
@@ -56,22 +56,12 @@ Material_Refractive::Material_Refractive(const Elite::RGBColor& diffuseColor)
 	m_DiffuseColor = diffuseColor;
 }
 
-Elite::RGBColor Material_Refractive::Shade(const HitRecord& hitRecord, const Elite::FVector3& l, const Elite::FVector3& v, bool& isShade, const Elite::FVector3& incidentRay) const
+Elite::RGBColor Material_Refractive::Shade(const HitRecord& hitRecord, const Elite::FVector3& l, const Elite::FVector3& v, bool& isRefractive, const Elite::FVector3& incidentRay) const
 {
 	Elite::RGBColor trnColor = {0,0,0};
 	
-	//
-	Elite::FVector3 p = incidentRay;
-	auto tempNormal = hitRecord.normal;
-	double r = 1.0 / m_IndexOfRef;
-	double c = Elite::Dot(tempNormal, p);
-
-	if (c < 0.0)
-	{
-		c = -Elite::Dot(tempNormal, p);
-	}
-
-	Elite::FVector3 refractedVector = r * p + (r * c - sqrtf(1.0 - pow(r, 2.0) * (1.0 - pow(c, 2.0)))) * tempNormal;
+	//get the rafracted vector
+	Elite::FVector3 refractedVector = GetRefractedRay(hitRecord,incidentRay);
 
 	//the refracted ray
 	Ray refractedRay(hitRecord.hitPoint + (refractedVector * 0.01), hitRecord.hitPoint + refractedVector);
@@ -88,14 +78,8 @@ Elite::RGBColor Material_Refractive::Shade(const HitRecord& hitRecord, const Eli
 	{
 		Elite::FVector3 p2 = refractedRay.GetDirection();
 		p2 = Elite::GetNormalized(p2);
-		auto tempNormal2 = newHitRecord.normal;
-		double r2 = 1.0;
-		double c2 = Elite::Dot(tempNormal2, p2);
-		if (c2 < 0.0)
-		{
-			c2 = -Elite::Dot(tempNormal2, p2);
-		}
-		Elite::FVector3 refractedVector2 = r2 * p2 + (r2 * c2 - sqrtf(1.0 - pow(r2, 2.0) * (1.0 - pow(c2, 2.0)))) * tempNormal2;
+		//get the refracted vector
+		Elite::FVector3 refractedVector2 = GetRefractedRay(newHitRecord, p2);
 
 		//refracter ray
 		Ray refractedRay2(newHitRecord.hitPoint + (refractedVector2 * 0.01), newHitRecord.hitPoint + refractedVector2);
@@ -163,10 +147,112 @@ Elite::RGBColor Material_Refractive::Shade(const HitRecord& hitRecord, const Eli
 			}
 		}
 	}
-	isShade = true;
+	isRefractive = true;
 	trnColor = matColor + m_DiffuseColor * 0.1f;
 	return trnColor;
 }
+
+Elite::FVector3 Material_Refractive::GetRefractedRay(const HitRecord& hitRecord, const Elite::FVector3& incidentRay) const
+{
+	Elite::FVector3 p = incidentRay;
+	auto tempNormal = hitRecord.normal;
+	double r = 1.0 / m_IndexOfRef;
+	double c = Elite::Dot(tempNormal, p);
+
+	if (c < 0.0)
+	{
+		c = -Elite::Dot(tempNormal, p);
+	}
+	return r * p + (r * c - sqrtf(1.0 - pow(r, 2.0) * (1.0 - pow(c, 2.0)))) * tempNormal;
+}
+
+Material_Reflective::Material_Reflective(const Elite::RGBColor& diffuseColor)
+{
+	m_DiffuseColor = diffuseColor;
+}
+
+Elite::RGBColor Material_Reflective::Shade(const HitRecord& hitRecord, const Elite::FVector3& l, const Elite::FVector3& v, bool& isRefractive, const Elite::FVector3& incidentRay) const
+{
+	//get the reflected vector
+	Elite::FVector3 reflectedVector = incidentRay - (2 * Elite::Dot(incidentRay, hitRecord.normal) * hitRecord.normal);
+
+	//the reflected ray
+	Ray reflectedRay(hitRecord.hitPoint, hitRecord.hitPoint + reflectedVector);
+
+	//intersection test
+	Shape* closestObject = nullptr;
+	HitRecord closestHitRecord;
+	bool intersectionFound = HitObjects(reflectedRay, m_pShape, closestObject, closestHitRecord);
+
+
+	Elite::RGBColor matColor = { 0,0,0 };
+
+	if (intersectionFound)
+	{
+		if (closestObject->GetMaterial())
+		{
+			for (size_t i = 0; i < LightManager::GetInstance()->GetLights().size(); i++)
+			{
+				auto pLight = LightManager::GetInstance()->GetLights()[i];
+				//getting the Hitpoint direction and its length
+				Elite::FPoint3 hitPointWithOffset = closestHitRecord.hitPoint + (closestHitRecord.normal * 0.001f);
+				Elite::FVector3 direction = pLight->GetDirection(hitPointWithOffset);
+				float lengthDirection = Normalize(direction);
+
+				bool isPointVisible = true;
+				//if (closestObject->GetShapeType() != Shape::ShapeType::plane)
+				{
+					//checking for shadows
+					HitRecord tempHitRecord{};
+					tempHitRecord.isLightRay = true;
+					Ray lightRay;
+					lightRay.SetTMin(0.001f);
+					lightRay.SetTMax(lengthDirection);
+					lightRay.SetOrigin(hitPointWithOffset);
+					lightRay.SetDirection(direction);
+
+					for (Shape* pShape : Scenegraph::GetInstance()->GetShapes())
+					{
+						if (pShape->Hit(lightRay, tempHitRecord))
+						{
+							isPointVisible = false;
+						}
+					}
+				}
+				Elite::RGBColor Ergb;
+				if (isPointVisible)
+				{
+					float dotProduct = Dot(closestHitRecord.normal, direction);
+					if (dotProduct >= 0)
+					{
+						//light contribution
+						Ergb = pLight->GetIrradiance(closestHitRecord);
+						// calculate v for shade
+						Elite::FVector3 v = Elite::GetNormalized(Elite::FVector3(Elite::Renderer::m_Camera->GetPosition() - Elite::FVector3(hitPointWithOffset)));
+						bool temp = false;
+						auto shade = Ergb * closestHitRecord.pMaterial->Shade(closestHitRecord, direction, v, temp, incidentRay) * dotProduct;
+						if (temp)
+						{
+							matColor = shade;
+						}
+						else
+						{
+							matColor += Ergb * shade * dotProduct;
+							matColor.MaxToOne();
+						}
+						isRefractive = temp;
+					}
+				}
+				else
+				{
+					matColor = { 0,0,0 };
+				}
+			}
+		}
+	}
+	return matColor + m_DiffuseColor * 0.1f;
+}
+
 
 bool Material::HitObjects(const Ray& castRay, Shape* thisObject, Shape*& closestObject, HitRecord& hitRecord) const
 {
@@ -194,3 +280,4 @@ bool Material::HitObjects(const Ray& castRay, Shape* thisObject, Shape*& closest
 	}
 	return intersectionFound;
 }
+
--- Material.h
+++ Material.h
@@ -13,10 +13,11 @@ public:
 	Material(Material&&) noexcept = delete;
 	Material& operator=(const Material&) = delete;
 	Material& operator=(Material&&) noexcept = delete;
-	virtual Elite::RGBColor Shade(const HitRecord& hitRecord, const Elite::FVector3& l, const Elite::FVector3& v, bool& isShade, const Elite::FVector3& incidentRay) const = 0;
-	virtual void SetAllShapes(std::vector<Shape*> pAllShapes) {};
+	virtual Elite::RGBColor Shade(const HitRecord& hitRecord, const Elite::FVector3& l, const Elite::FVector3& v, bool& isRefractive, const Elite::FVector3& incidentRay) const = 0;
 	virtual void SetCurrShape(Shape* pShape) {};
 	bool HitObjects(const Ray& castRay, Shape* thisObject, Shape*& closestObject, HitRecord& hitRecord) const;
+	void SetIndexOfRefraction(double ior) { m_IndexOfRef = ior; }
+
 	//Datamembers
 	Elite::RGBColor m_DiffuseColor{ 0,0,0 };
 	float m_DiffuseReflectance{ 0 };
@@ -34,7 +35,7 @@ public:
 	Material_Lambert(Material_Lambert&&) noexcept = delete;
 	Material_Lambert& operator=(const Material_Lambert&) = delete;
 	Material_Lambert& operator=(Material_Lambert&&) noexcept = delete;
-	Elite::RGBColor Shade(const HitRecord& hitRecord, const Elite::FVector3& l, const Elite::FVector3& v, bool& isShade, const Elite::FVector3& incidentRay) const override;
+	Elite::RGBColor Shade(const HitRecord& hitRecord, const Elite::FVector3& l, const Elite::FVector3& v, bool& isRefractive, const Elite::FVector3& incidentRay) const override;
 };
 
 class Material_LambertPhong final : public Material
@@ -47,7 +48,7 @@ public:
 	Material_LambertPhong(Material_LambertPhong&&) noexcept = delete;
 	Material_LambertPhong& operator=(const Material_LambertPhong&) = delete;
 	Material_LambertPhong& operator=(Material_LambertPhong&&) noexcept = delete;
-	Elite::RGBColor Shade(const HitRecord& hitRecord, const Elite::FVector3& l, const Elite::FVector3& v, bool& isShade, const Elite::FVector3& incidentRay) const override;
+	Elite::RGBColor Shade(const HitRecord& hitRecord, const Elite::FVector3& l, const Elite::FVector3& v, bool& isRefractive, const Elite::FVector3& incidentRay) const override;
 private:
 	float m_SpecularReflectance;
 	float m_PhongExponent;
@@ -63,7 +64,7 @@ public:
 	Material_CookTorrance(Material_CookTorrance&&) noexcept = delete;
 	Material_CookTorrance& operator=(const Material_CookTorrance&) = delete;
 	Material_CookTorrance& operator=(Material_CookTorrance&&) noexcept = delete;
-	Elite::RGBColor Shade(const HitRecord& hitRecord, const Elite::FVector3& l, const Elite::FVector3& v, bool& isShade, const Elite::FVector3& incidentRay) const override;
+	Elite::RGBColor Shade(const HitRecord& hitRecord, const Elite::FVector3& l, const Elite::FVector3& v, bool& isRefractive, const Elite::FVector3& incidentRay) const override;
 private:
 
 	bool m_IsMetal;
@@ -81,6 +82,20 @@ public:
 	Material_Refractive(Material_Refractive&&) noexcept = delete;
 	Material_Refractive& operator=(const Material_Refractive&) = delete;
 	Material_Refractive& operator=(Material_Refractive&&) noexcept = delete;
-	Elite::RGBColor Shade(const HitRecord& hitRecord, const Elite::FVector3& l, const Elite::FVector3& v, bool& isShade, const Elite::FVector3& incidentRay) const override;
+	Elite::RGBColor Shade(const HitRecord& hitRecord, const Elite::FVector3& l, const Elite::FVector3& v, bool& isRefractive, const Elite::FVector3& incidentRay) const override;
+private:
+	Elite::FVector3 GetRefractedRay(const HitRecord& hitRecord, const Elite::FVector3& incidentRay) const;
+};
 
+class Material_Reflective final : public Material
+{
+public:
+	Material_Reflective(const Elite::RGBColor& diffuseColor);
+	void SetCurrShape(Shape* pShape) override { m_pShape = pShape; };
+	~Material_Reflective() = default;
+	Material_Reflective(const Material_Reflective&) = delete;
+	Material_Reflective(Material_Reflective&&) noexcept = delete;
+	Material_Reflective& operator=(const Material_Reflective&) = delete;
+	Material_Reflective& operator=(Material_Reflective&&) noexcept = delete;
+	Elite::RGBColor Shade(const HitRecord& hitRecord, const Elite::FVector3& l, const Elite::FVector3& v, bool& isRefractive, const Elite::FVector3& incidentRay) const override;
 };
